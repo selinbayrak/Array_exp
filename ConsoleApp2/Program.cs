b870790b4385acbcc4662cc39d036a5830ad83b6
AYSE BAYRAK <AYSE BAYRAK@LAPTOP-J2JFAAN1>
2024-11-18 19:44:45 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] ögrenciler = { "Safiye", "Eymen", "Safiye", "Bekir", "Rümeysa", "Safiye", "Berat", "Safiye" };
+
+int baslangicİndexi = Array.IndexOf(ögrenciler, "Safiye");
+int sonucIndexi = Array.LastIndexOf(ögrenciler, "Safiye");
+
+Console.WriteLine(baslangicİndexi);
+
+while (baslangicİndexi != sonucIndexi) { 
+
+    int yenibaslangicindexi = Array.IndexOf(ögrenciler, "Safiye", baslangicİndexi+1);
+
+    Console.WriteLine(yenibaslangicindexi);
+
+    baslangicİndexi = yenibaslangicindexi;
+   
+}
